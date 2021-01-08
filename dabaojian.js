21db8bf4c1c84cace00d48c3bfece6b9cf88535f
liheng <1061555401@qq.com>
2021-01-09 00:21:14 +0800

    异步测试

--- dabaojian.js
+++ dabaojian.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export function baojian1(money) {
   return money >= 200 ? '至尊享受' : '基本按摩'
 }
@@ -5,3 +7,15 @@ export function baojian1(money) {
 export function baojian2(money) {
   return money >= 1000 ? '双人享受' : '单人按摩'
 }
+
+export const fetchData = (callback) => {
+  setInterval(() => {
+    callback && callback(100)
+  }, 1000);
+}
+
+export const rePromise = () => new Promise((resolve, reject) => {
+  setInterval(() => {
+    resolve(1)
+  }, 1000);
+})
