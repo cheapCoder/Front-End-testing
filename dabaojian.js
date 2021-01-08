d6a0a2cc92726775ef73819b69b8ee18c2e00e10
liheng <1061555401@qq.com>
2021-01-08 22:11:02 +0800

    jest匹配器

--- dabaojian.js
+++ dabaojian.js
@@ -1,11 +1,7 @@
-function baojian1(money) {
+export function baojian1(money) {
   return money >= 200 ? '至尊享受' : '基本按摩'
 }
 
-function baojian2(money) {
+export function baojian2(money) {
   return money >= 1000 ? '双人享受' : '单人按摩'
-}
-
-module.exports = {
-  baojian1, baojian2
 }
