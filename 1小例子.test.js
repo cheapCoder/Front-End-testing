0a76d914c3a486739756d44a3d755bb8d3b2ca55
liheng <1061555401@qq.com>
2021-01-13 10:43:06 +0800

    修改main

--- 1小例子.test.js
+++
@@ -1,8 +0,0 @@
-import { baojian1, baojian2 } from './dabaojian.js'
-
-test('test-1', () => {
-  expect(baojian1(300)).toBe('至尊享受')
-})
-// test('test-2', () => {
-//   expect(baojian2(2000)).toBe('人享受')
-// })
